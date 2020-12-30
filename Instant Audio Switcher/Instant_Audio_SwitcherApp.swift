009efa586cd72775e891007c34b294576b903ba6
Jed Fox <git@jedfox.com>
2020-12-30 15:48:46 -0500

    Initial Commit

---
+++ Instant Audio Switcher/Instant_Audio_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instant_Audio_SwitcherApp.swift
+//  Instant Audio Switcher
+//
+//  Created by Jed Fox on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Instant_Audio_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
