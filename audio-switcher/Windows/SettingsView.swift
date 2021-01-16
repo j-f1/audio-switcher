0e9b0c72adb33109388a727911809f31b6613633
Jed Fox <git@jedfox.com>
2021-01-16 15:19:03 -0500

    settings ui/copy tweaks

--- audio-switcher/Windows/SettingsView.swift
+++ audio-switcher/Windows/SettingsView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 import Defaults
 import LaunchAtLogin
 
+let openMenuText = Text("open the menu and change your settings.")
+let setOutputText = { (name: String?) in Text("send music, videos, and sounds to “\(name ?? "<unknown device>")\u{202d}.”") }
+
+let clickIconToText = { (icon: String) in Text("Click the ") + Text(Image(systemName: icon)) + Text(" icon in the menu bar to ") }
+let altClickIconToText = Text("Option-click, control-click, or right-click the menu item to ")
+
 struct SettingsView: View {
   @Default(.showInDock) var showInDock
   @Default(.clickToActivate) var clickToActivate
@@ -24,7 +30,7 @@ struct SettingsView: View {
       Toggle("Play sound after connecting", isOn: $playSound)
       LaunchAtLogin.Toggle()
       Toggle(isOn: $clickToActivate) {
-        VStack(alignment: .leading, spacing: 2) {
+        VStack(alignment: .leading, spacing: 5) {
           HStack(spacing: 0) {
             Text("Click menu item to activate “").fixedSize()
             Text(selectedDevice ?? "")
@@ -32,17 +38,21 @@ struct SettingsView: View {
               .truncationMode(.tail)
             Text("”").fixedSize()
           }
-          (
-            Text("")
-              + Text("When enabled, click the ") + Text(Image(systemName: selectedIcon)) + Text(" ")
-              + Text("icon in the menu bar to send music, videos, and sounds to “\(selectedDevice ?? "<unknown device>")\u{202d}.”\n")
-              + Text("Option-click, control-click, or right-click the menu item to open the menu and change your settings.")
-          ).foregroundColor(.secondary)
+          ZStack(alignment: .topLeading) {
+            VStack(alignment: .leading, spacing: 5) {
+              clickIconToText(selectedIcon) + setOutputText(selectedDevice)
+              altClickIconToText + openMenuText
+            }.opacity(clickToActivate ? 1 : 0)
+            VStack(alignment: .leading, spacing: 5) {
+              clickIconToText(selectedIcon) + openMenuText
+              altClickIconToText + setOutputText(selectedDevice)
+            }.opacity(clickToActivate ? 0 : 1)
+          }.foregroundColor(.secondary)
            .imageScale(.small)
            .font(.body)
            .fixedSize(horizontal: false, vertical: true)
         }
-      }
+      }.padding(.bottom, -4)
 
       Group {
         Divider().padding(.vertical, 8)
