38c21e92f037231cbcc365f54396f23ea57ff36d
Jed Fox <git@jedfox.com>
2021-02-28 16:09:53 -0500

    wip

---
+++ audio-switcher/Windows/WelcomeWindow.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeWindow.swift
+//  Instant Audio Switcher
+//
+//  Created by Jed Fox on 2/28/21.
+//
+
+import SwiftUI
+
+struct WelcomeWindow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeWindow_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeWindow()
+    }
+}
